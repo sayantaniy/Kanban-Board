5f5fbcf5cd473a2bfcf81fb60064cf6e18bc5e0c
sayantaniy <sayantanimanna139@gmail.com>
2025-12-14 01:26:07 +0530

    Added Modal add task

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
+let dragElement = null;
 
 const tasks = document.querySelectorAll(".task"); //creates an array which consists the tasks
 
 tasks.forEach(task =>{   //goes to every element in tasks array
     task.addEventListener('drag',(e)=>{  //whenever a task is dragged
-
+       dragElement = task;
     })
 })
 
@@ -22,9 +23,34 @@ function addDragEventsOnColumn (column) {
         e.preventDefault();
         column.classList.remove("hover-over");
     })
+
+    column.addEventListener('dragover', (e)=>{
+        e.preventDefault(); //allow drop
+        
+    })
+
+    column.addEventListener('drop', (e)=>{
+        e.preventDefault();
+        console.log("Dropped", dragElement, column);
+        column.appendChild(dragElement);
+        column.classList.remove("hover-over");
+    })
 }
 
 addDragEventsOnColumn(todo);
 addDragEventsOnColumn(progress);
 addDragEventsOnColumn(done);
 
+// Modal related Logic
+
+const toggleModalButton = document.querySelector("#toggle-modal");
+const modal = document.querySelector(".modal");
+const modalbg = document.querySelector(".modal .bg");
+
+toggleModalButton.addEventListener("click", () => {
+    modal.classList.toggle("active");
+});
+
+modalbg.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
