61c3d6f7c9070f53488b2a15fc335b353c7420b4
sayantaniy <sayantanimanna139@gmail.com>
2025-12-15 22:32:38 +0530

    Optimized Code Logic

--- script.js
+++ script.js
@@ -6,25 +6,56 @@ const done = document.querySelector("#done");
 const columns = [todo, progress, done];
 let dragElement = null;
 
+function addTask(title,desc,column){
+
+    const div = document.createElement("div");
+    div.classList.add('task');
+    div.setAttribute('draggable', 'true');
+    div.innerHTML = `<h3>${title}</h3>
+                    <p> ${desc} </p>
+                    <button> Delete </button>
+    `;
+    column.appendChild(div);
+    div.addEventListener('drag', (e)=>{
+        dragElement = div;
+    })
+    return div;
+}
+
+
+function updateTaskCount() {
+    columns.forEach(col=>{
+
+        const tasks = col.querySelectorAll(".task");
+        const count = col.querySelector(".right");
+
+        tasksData[col.id] = Array.from(tasks).map(task => {  //Dynamic obj key tasksData[todo]
+            return {
+                title: task.querySelector("h3").innerText,
+                description: task.querySelector("p").innerText
+            }
+        })
+        localStorage.setItem("tasks", JSON.stringify(tasksData));
+
+        count.innerText = `Count : ${tasks.length}`;
+    })
+}
+
+
 if(localStorage.getItem("tasks")) {
     const data = JSON.parse(localStorage.getItem("tasks")) || {};  //returns an object hence for in loop
     console.log(data); //logs an object
 
     for(const key in data){
         const column = document.querySelector(`#${key}`);  //here key is todo,progress,done
+
         data[key].forEach(task => {
-            const div = document.createElement("div");
-            div.classList.add('task');
-            div.setAttribute('draggable', 'true');
-            div.innerHTML = `<h3>${task.title}</h3>
-                            <p> ${task.description} </p>
-                            <button> Delete </button>
-            `;
-            column.appendChild(div);
-            div.addEventListener('drag', (e)=>{
-                dragElement = div;
-            })
+            addTask(task.title,task.description,column);
         })
+
+        updateTaskCount();
+        
+        
     }
 }
  
@@ -63,22 +94,8 @@ function addDragEventsOnColumn (column) {
         column.appendChild(dragElement);
         column.classList.remove("hover-over");
 
+       updateTaskCount();
        
-        columns.forEach(col=>{
-
-            const tasks = col.querySelectorAll(".task");
-            const count = col.querySelector(".right");
-    
-            tasksData[col.id] = Array.from(tasks).map(task => {  //Dynamic obj key tasksData[todo]
-                return {
-                    title: task.querySelector("h3").innerText,
-                    description: task.querySelector("p").innerText
-                }
-            })
-            localStorage.setItem("tasks", JSON.stringify(tasksData));
-    
-            count.innerText = tasks.length;
-        })
     })
 }
 
@@ -107,38 +124,11 @@ addTaskButton.addEventListener("click", () => {
     const taskTitle = document.querySelector("#task-title-input").value;
     const taskDesc = document.querySelector("#task-desc-input").value;
     
-    const div = document.createElement("div");
-    div.classList.add('task');
-    div.setAttribute('draggable', 'true');
-
-    div.innerHTML = `<h3>${taskTitle}</h3>
-                    <p> ${taskDesc} </p>
-                    <button> Delete </button>
-    `;
-
-    div.addEventListener('drag', (e)=>{
-        dragElement = div;
-    })
-
-    todo.appendChild(div);
+    addTask(taskTitle, taskDesc, todo);  //new added task is added to first column ToDo
 
 
     // Save to local storage
-    columns.forEach(col=>{
-
-        const tasks = col.querySelectorAll(".task");
-        const count = col.querySelector(".right");
-
-        tasksData[col.id] = Array.from(tasks).map(task => {  //Dynamic obj key tasksData[todo]
-            return {
-                title: task.querySelector("h3").innerText,
-                description: task.querySelector("p").innerText
-            }
-        })
-        localStorage.setItem("tasks", JSON.stringify(tasksData));
-
-        count.innerText = tasks.length;
-    })
+    updateTaskCount();
     
     modal.classList.remove("active");
 
